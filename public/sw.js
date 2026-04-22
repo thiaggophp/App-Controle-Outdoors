5a119155997aed5ac6389c44d4564fdc1363bad0
Thiago <thiago@sygmasistemas.com.br>
2026-04-22 14:42:43 -0300

    feat: login preenche usuário e senha automaticamente (sem checkbox)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="outdoor-v6";
+const CACHE="outdoor-v7";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/","/index.html"])));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{
